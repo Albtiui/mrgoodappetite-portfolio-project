97b94e455af498b1ab096ad3dcbb91441d0cb6d1
Albtiui Mohamed <135072672+Albtiui@users.noreply.github.com>
2024-06-27 22:13:42 +0000

    a

---
+++ src/product.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    images: {
+        type: [String],
+        required: true,
+        data: Buffer,
+
+        
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    description : {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+
+})
+
+export default  mongoose.models.Product || mongoose.model('Product', productSchema)
